d0fbdd77d17f42b136691498f71c2c8d4d7c7c87
Salvatore <97906755+nevpit@users.noreply.github.com>
2025-06-02 20:53:35 -0400

    Add NDVI computation utility

--- processing/__init__.py
+++ processing/__init__.py
@@ -2,6 +2,7 @@
 
 from .lidar import write_geotiff, generate_lrm, generate_hillshade, build_dtm
 from .image import to_uint8
+from .sat import compute_ndvi
 
 __all__ = [
     "write_geotiff",
@@ -9,5 +10,6 @@ __all__ = [
     "generate_hillshade",
     "build_dtm",
     "to_uint8",
+    "compute_ndvi",
 ]
 
